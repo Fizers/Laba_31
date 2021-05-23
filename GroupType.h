88e9c2cb44ffc52a77c862c719f519c26506af23
Katerina <fizerskat@gmail.com>
2021-05-24 02:32:54 +0800

    Поправка

--- GroupType.h
+++ GroupType.h
@@ -11,6 +11,7 @@ public:
 private:
     void getTypeSize(const QString& path, QMap<QString, qint64>& TypeList);
     QMap<QString, double> getTypePercent(qint64& totalSize, QMap<QString, qint64>& TypeList) const;
+    QList<QPair<double, QString>> sortPercent(const QMap<QString, double>& TypePercant);
     void PrintTypeAllInf(const QMap<QString, qint64>& FileTypesList, const QList<QPair<double, QString>> TypePercant) const;
 };
 
